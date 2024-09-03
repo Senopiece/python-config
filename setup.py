ba2d697b0db79eb06a1ddceae4810742eb99ddd9
Nabuki <shichiromail@gmail.com>
2024-09-03 19:06:29 +0300

    rename again

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="python_config",
+    name="configurable",
     version="1.0",
     description="Library for loading yml configurations",
     author="Vitaly Mahonin",
