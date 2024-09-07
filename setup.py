b213d523384416ecd63ee7aeef2b5b0eece72a17
Nabuki <shichiromail@gmail.com>
2024-09-07 21:19:44 +0300

    add readme to pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="configurik",
     version="1.2",
@@ -11,4 +14,7 @@ setup(
         "python-dotenv>=1.0.1,<2.0.0",
     ],
     python_requires=">=3.8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Senopiece/python-config",
 )
