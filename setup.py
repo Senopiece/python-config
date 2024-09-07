e2bb7e60db8df6b12954966c915841bedb229ad6
Nabuki <shichiromail@gmail.com>
2024-09-07 21:22:34 +0300

    up upper

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="configurik",
-    version="1.2.1",
+    version="1.3",
     description="Library for loading yml configurations",
     author="Vitaly Mahonin",
     author_email="nabuki@vk.com",
