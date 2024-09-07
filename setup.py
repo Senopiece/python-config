ff76dbe19772f0c44cda8feb2abc022dab4cae69
Nabuki <shichiromail@gmail.com>
2024-09-07 17:24:36 +0300

    rebrand

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="configurable",
+    name="configurik",
     version="1.2",
     description="Library for loading yml configurations",
     author="Vitaly Mahonin",
