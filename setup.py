e29a4bcb7b5d9611c17fec37321604c13b2794a9
Nabuki <shichiromail@gmail.com>
2024-09-21 19:41:46 +0300

    add publish script and update dependencies

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="configurik",
-    version="1.3",
+    version="1.3.1",
     description="Library for loading yml configurations",
     author="Vitaly Mahonin",
     author_email="nabuki@vk.com",
     packages=find_packages(),
     install_requires=[
         "python-dotenv>=1.0.1,<2.0.0",
+        "PyYAML>=6.0.1,<7.0.0",
     ],
     python_requires=">=3.8",
     long_description=long_description,
